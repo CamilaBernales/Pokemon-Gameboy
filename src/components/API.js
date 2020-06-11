6d8dd0f90408a76e84c25fed1296161c526300f0
Camila Bernales <camilabernales09@gmail.com>
2020-06-11 02:32:21 -0300

    terminadito

--- src/components/API.js
+++ src/components/API.js
@@ -24,7 +24,17 @@ const API = () => {
   return (
     <div className="box">
       <div className="gameboy">
+        <div class="top">
+          <span>◁ OFF·ON ▷</span>
+        </div>
         <div className="screen">
+          <div className="screen-top">
+            <span>DOT MATRIX WITH STEREO SOUND</span>
+          </div>
+          <div class="battery">
+            <div class="led"></div>
+            <p>Baterry</p>
+          </div>
           <div className="game-screen">
             <img
               src={
@@ -37,7 +47,43 @@ const API = () => {
             <h1>{pokemon.name}</h1>
           </div>
         </div>
-        <button onClick={() => consultarApi()}>next</button>
+        <div class="marca">
+          <p>Nintendo</p>
+        </div>
+        <div className="controles">
+          <div class="cross">
+            <div class="cursor up"></div>
+            <div class="cursor left"></div>
+            <div class="cursor center">
+              <div class="circle"></div>
+            </div>
+            <div class="cursor right"></div>
+            <div class="cursor down"></div>
+          </div>
+        </div>
+        <div className="btn-rd">
+          <button className="btn-rd1"></button>
+          <button className="btn-rd2"></button>
+        </div>
+        <div className="buttons">
+          <div className="start-container">
+            <button
+              onClick={() => consultarApi()}
+              className="start-btn"
+            ></button>
+          </div>
+          <div className="select-container">
+            <button className="select-btn"></button>
+          </div>
+        </div>
+        <div class="audio">
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+        </div>
       </div>
     </div>
   );
